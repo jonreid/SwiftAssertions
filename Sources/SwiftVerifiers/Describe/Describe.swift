0882c5dbead70859ceedde7053e732e0d910cfc2
Jon Reid <jon@qualitycoding.org>
2025-06-06 21:43:55 -0700

    . r Move DescriberChain below describe(_:)

--- Sources/SwiftVerifiers/Describe/Describe.swift
+++ Sources/SwiftVerifiers/Describe/Describe.swift
@@ -2,6 +2,10 @@
 // Copyright 2025 Jonathan M. Reid. https://github.com/jonreid/SwiftVerifiers/blob/main/LICENSE.txt
 // SPDX-License-Identifier: MIT
 
+public func describe(_ value: Any) -> String {
+    return DescriberChain.head.handle(value)
+}
+
 private enum DescriberChain {
     static let head: Describer = {
         let fallback = FallbackDescriber()
@@ -9,8 +13,4 @@ private enum DescriberChain {
         let optionalHandler = OptionalDescriber(successor: stringHandler)
         return optionalHandler
     }()
-}
-
-public func describe(_ value: Any) -> String {
-    return DescriberChain.head.handle(value)
 }
