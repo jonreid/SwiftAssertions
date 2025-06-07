cccbaa1e9e7788f8432d65dcd427def6cae2d0a2
Jon Reid <jon@qualitycoding.org>
2025-06-06 21:41:21 -0700

    . r Move functions into methods

--- Sources/SwiftVerifiers/Describe/StringDescriber.swift
+++ Sources/SwiftVerifiers/Describe/StringDescriber.swift
@@ -6,27 +6,28 @@ final class StringDescriber: Describer, @unchecked Sendable {
     override func willHandle(_ value: Any) -> Bool {
         value is String
     }
+    
     override func describe(_ value: Any) -> String {
         toFormattedString(value as! String)
     }
-}
 
-private func toFormattedString(_ unformatted: String) -> String {
-    let formattedChars = unformatted.map { escapeSpecialCharacter($0) }
-    return "\"\(formattedChars.joined())\""
-}
+    private func toFormattedString(_ unformatted: String) -> String {
+        let formattedChars = unformatted.map { escapeSpecialCharacter($0) }
+        return "\"\(formattedChars.joined())\""
+    }
 
-private func escapeSpecialCharacter(_ char: Character) -> String {
-    switch char {
-    case "\"":
-        return "\\\""
-    case "\n":
-        return "\\n"
-    case "\r":
-        return "\\r"
-    case "\t":
-        return "\\t"
-    default:
-        return String(char)
+    private func escapeSpecialCharacter(_ char: Character) -> String {
+        switch char {
+        case "\"":
+            return "\\\""
+        case "\n":
+            return "\\n"
+        case "\r":
+            return "\\r"
+        case "\t":
+            return "\\t"
+        default:
+            return String(char)
+        }
     }
 }
